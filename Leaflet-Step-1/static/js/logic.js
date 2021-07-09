d0460ecec614ff9a585c6df43b095bffeda5253a
Julia Brunett <julia.h.brunett@gmail.com>
2021-07-09 11:53:45 -0700

    Added circles on map

--- Leaflet-Step-1/static/js/logic.js
+++ Leaflet-Step-1/static/js/logic.js
@@ -1,43 +1,24 @@
-// // Map Object
-// var myMap = L.map("map", {
-//     center: [39.8283, -98.5795], 
-//     zoom: 4
-// });
+// Map Object
+var myMap = L.map("map", {
+    center: [39.8283, -98.5795], 
+    zoom: 4
+});
 
-// // Tile Layer
-// L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
-//     attribution: "© <a href='https://www.mapbox.com/about/maps/'>Mapbox</a> © <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a> <strong><a href='https://www.mapbox.com/map-feedback/' target='_blank'>Improve this map</a></strong>",
-//     tileSize: 512,
-//     maxZoom: 18,
-//     zoomOffset: -1,
-//     id: "mapbox/streets-v11",
-//     accessToken: API_KEY
-// }).addTo(myMap);
+// Tile Layer
+L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
+    attribution: "© <a href='https://www.mapbox.com/about/maps/'>Mapbox</a> © <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a> <strong><a href='https://www.mapbox.com/map-feedback/' target='_blank'>Improve this map</a></strong>",
+    tileSize: 512,
+    maxZoom: 18,
+    zoomOffset: -1,
+    id: "mapbox/streets-v11",
+    accessToken: API_KEY
+}).addTo(myMap);
 
 // Earthquake data link
 var url = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson";
 
 function sizeCircle(magnitude) {
-    if (magnitude >= 8) {
-        color = "red";
-    }
-    else if (magnitude <= 7.9 && magnitude >= 7) {
-        color = "orange";
-    }
-    else if (magnitude <= 6.9 && magnitude >= 6) {
-        color = "yellow";
-    }
-    else if (magnitude <= 5.9 && magnitude >= 5) {
-        color = "green";
-    }
-    else if (magnitude <= 4.9 && magnitude >= 4) {
-        color = "blue";
-    }
-    else if (magnitude <= 3.9) {
-        color = "grey";
-    };
-
-    return size;
+    return magnitude * 4;
 };
 
 function colorCircle(depth) {
@@ -67,7 +48,7 @@ d3.json(url).then(data => {
     console.log(data);
 
     // Create a cluster group
-    var cluster_group = L.markerClusterGroup();
+    // var cluster_group = L.markerClusterGroup();
     var features = data.features;
     var depth_array = [];
 
@@ -91,12 +72,12 @@ d3.json(url).then(data => {
         // cluster_group.addLayer((L.marker([coordinates[1], coordinates[0]]))
         //     .bindPopup(`<h3>${place}</h3><br/>Magnitude: ${magnitude}`));
 
-        // cluster_group.addLayer((L.circle([coordinates[1], coordinates[0]], {
-        //     color: "white",
-        //     fillColor: colorCircle(magnitude),
-        //     fillOpactiy: 0.6,
-        //     radius: sizeCircle()
-        // })).bindPopup(`<h3>${place}</h3><br/>Magnitude: ${magnitude}`));
+        L.circleMarker([coordinates[1], coordinates[0]], {
+            color: "white",
+            fillColor: colorCircle(depth),
+            fillOpactiy: 0.6,
+            radius: sizeCircle(magnitude)
+        }).bindPopup(`<h3>${place}</h3><br/>Magnitude: ${magnitude}`).addTo(myMap);
 
         // console.log(coordinates);
 
